a4a1840dd0bc9fe5403b7974190a5bc0e8756b89
Karrar Qasim <Karawr1996@gmail.com>
2023-02-20 15:39:17 -0700

    Started app component, and bookcontainer

---
+++ src/components/BookContainer/BookContainer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './BookContainer.css'
+
+const BookContainer = (props) => {
+
+    return(
+        <h2>Books, yeah</h2>
+    )
+}
+
+export default BookContainer;
