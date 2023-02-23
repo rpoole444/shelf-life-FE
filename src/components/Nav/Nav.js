7bbd369087041f780347be1da20a57692b9f26b7
John Ammon <ammonj78@gmail.com>
2023-02-22 18:56:11 -0700

    Add article for home page

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,7 +2,21 @@ import React from 'react';
 import './Nav.css';
 
 const Nav = () => {
-      
+  return (
+    <div>
+      <h1>ShelfLife</h1>
+      <div className="book-icon-container">
+        <img src="book.png" alt="book" className="book-icon" />
+        <article className="welcome-box">
+          <p>
+            Welcome to Shelf Life! Take a look at our selection of
+            recommendation, and click the "Learn More" button to learn more
+            about the book and store your favorites!{" "}
+          </p>
+        </article>
+      </div>
+    </div>
+  );
   
     
 }
