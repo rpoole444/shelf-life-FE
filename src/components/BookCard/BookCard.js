6d73557691fb6409067ea6d76e59f8930482a739
Reid Poole <poole.reid@gmail.com>
2023-02-22 22:31:31 -0600

    add Style and color to Main Page

--- src/components/BookCard/BookCard.js
+++ src/components/BookCard/BookCard.js
@@ -1,22 +1,23 @@
 import React from "react";
-import { Link } from 'react-router-dom';
-import './BookCard.css';
+import { Link } from "react-router-dom";
+import "./BookCard.css";
 
-const BookCard = ({title, author, bookImage, isbn, id, recommendedBy }) => {
-    return (
-      <section className="book-card">
-        <img className="book-cover" src={bookImage} alt={`${title} cover`}/>
-        <div className="book-side-panel">
-          <div className="book-side-panel-banner">
-            <h2 className="book-title">{title}</h2>
-            <p className="book-author">{author}</p>
-            <p>Recommended By: {recommendedBy}</p>
-          </div>
-          <Link key={id} to={`/${isbn}/`} className="learn-more-btn">Learn More</Link>
+const BookCard = ({ title, author, bookImage, isbn, id, recommendedBy }) => {
+  return (
+    <section className="book-card">
+      <img className="book-cover" src={bookImage} alt={`${title} cover`} />
+      <div className="book-side-panel">
+        <div className="book-side-panel-banner">
+          <h2 className="book-title">{title}</h2>
+          <p className="book-author">{author}</p>
+          <p>Recommended By: {recommendedBy}</p>
         </div>
-      </section>
-    )
-}
-
+        <Link key={id} to={`/${isbn}/`} className="learn-more-btn">
+          Learn More
+        </Link>
+      </div>
+    </section>
+  );
+};
 
 export default BookCard;
--- src/components/BookContainer/BookContainer.js
+++ src/components/BookContainer/BookContainer.js
@@ -1,26 +1,23 @@
 import React from "react";
 import BookCard from "../BookCard/BookCard";
-import './BookContainer.css';
+import "./BookContainer.css";
 
 const BookContainer = ({ allBooks }) => {
-    const bookCards = allBooks.map((book) => {
-      return (
-        <BookCard
-          key={book.id}
-          id={book.id}
-          isbn={book.isbn}
-          title={book.title}
-          bookImage={book.book_image}
-          author={book.author}
-          recommendedBy={book.recommendedBy}
-        />
-      )
-    })
+  const bookCards = allBooks.map((book) => {
+    console.log(book.recommended_by);
     return (
-      <section className="book-container">
-        {bookCards}
-      </section>
-    )
-  }
+      <BookCard
+        key={book.id}
+        id={book.id}
+        isbn={book.isbn}
+        title={book.title}
+        bookImage={book.book_image}
+        author={book.author}
+        recommendedBy={book.recommended_by}
+      />
+    );
+  });
+  return <section className="book-container">{bookCards}</section>;
+};
 
 export default BookContainer;
