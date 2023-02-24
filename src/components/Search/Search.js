1f9a8ac8e8a2d3bc534610d4d6b5f130392f6c36
John Ammon <ammonj78@gmail.com>
2023-02-24 12:14:59 -0700

    Added search component

---
+++ src/components/Search/Search.js
@@ -0,0 +1,19 @@
+import React,{ Component } from "react";
+import { Link } from 'react-router-dom'
+
+class Search extends Component {
+    constructor(){
+        super()
+        this.state={
+            top100: '',
+            recomended: ''
+        }
+    }
+
+
+
+
+
+
+}
+export default Search
