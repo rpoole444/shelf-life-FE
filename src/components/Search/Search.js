96bb54c94f7ed2ee561c0e51d1c275dce63011d0
John Ammon <ammonj78@gmail.com>
2023-02-24 12:15:57 -0700

    Created search inputs

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -12,6 +12,34 @@ class Search extends Component {
 
 
 
+    render(){
+        return(
+            <container>
+                <form>
+                    <label htmlFor='searchTop'>Search Top 100</label>
+                    <input
+                        type='text'
+                        id='searchTop'
+                        placeholder='Enter Title Here'
+
+                    />
+                    <lable htmlFor='recomended'>Recomended</lable>
+                    <input
+                        type='text'
+                        id='recomended'
+                        placeholder='Enter Title Here'
+                    />
+                    <button>Add Book</button>
+                </form>
+                <div>
+                    <Link className="landing-page-link" to="/">
+                        <h1>Return Home »</h1>
+                    </Link>
+                </div>
+                
+            </container>
+        )
+    }
 
 
 
